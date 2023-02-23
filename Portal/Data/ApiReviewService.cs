59dda7805eec2e6a6c65eca23422a294f0c25f13
Christof <christof_sprenger@hotmail.com>
2023-02-23 14:35:01 -0800

    add missing service

---
+++ Portal/Data/ApiReviewService.cs
@@ -0,0 +1,48 @@
+using ADO;
+
+namespace Model;
+
+internal class ApiReviewService : IDisposable
+{
+
+    const string PORTAL_QUERY = "07f74a85-6eea-4c89-ae8a-ca0da83a9734";
+    // const string PORTAL_QUERY = "4211f01e-6749-438c-8390-bb8723b8e9b6"; // me
+
+    public ApiReviewService(IConfiguration config)
+    {
+        var personalAccessToken = config["ADO:PersonalAccessToken"];
+        if (personalAccessToken == null)
+        {
+            throw new Exception("ADO:PersonalAccessToken configuration required");
+        }
+        client = AdoHttpClient.Create(personalAccessToken);
+    }
+
+    private readonly AdoHttpClient client;
+
+    public void Dispose()
+    {
+        ((IDisposable)client).Dispose();
+    }
+
+
+    internal async Task<IEnumerable<ApiReviewRequest>> GetApiReviewRequests()
+    {
+        var result = await client.GetWorkItemQueryResultAsync(PORTAL_QUERY);
+        // work item queries contain only IDs
+        var reviewRequests = await Task.WhenAll(result.WorkItems!
+            .AsParallel()
+            .Select(GetApiReviewRequestFromRef)
+        );
+
+        return reviewRequests.OrderBy(r => r.WorkItemId) ?? Enumerable.Empty<ApiReviewRequest>();
+    }
+
+    private async Task<ApiReviewRequest> GetApiReviewRequestFromRef(WorkItemReference wiRef)
+    {
+        var wi = await client.GetWorkItemAsync(wiRef.Id)!;
+        var pr = await client.GetPullRequestAsync(wi.GetPullRequestId())!;
+
+        return ApiReviewRequest.From(wi, pr);
+    }
+}
